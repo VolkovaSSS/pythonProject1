305da402be0c30ae1f8f41d667d6ef16261290f5
Sv <VolkovaSVG@mail.ru>
2025-10-20 20:10:00 +0400

    ф-ция: mask_account_card

--- src/masks.py
+++ src/masks.py
@@ -29,5 +29,5 @@ def get_mask_account(account_number: int) -> str:
     return "**" + str(account_number)[-4:]
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+# print(get_mask_card_number(7000792289606361))
+# print(get_mask_account(73654108430135874305))
---
+++ src/widget.py
@@ -0,0 +1,25 @@
+import masks
+
+def count_number_len(account_card: str) -> int:
+    """вычисляет количество цифр карты(счета)"""
+    last_space_position = account_card.rfind(" ")
+    if last_space_position == -1:
+        return 0
+    else:
+        return len(account_card) - last_space_position - 1
+
+
+def mask_account_card(account_card: str) -> str:
+    """Маскирует номер карты или счета"""
+
+    number_len = count_number_len(account_card)
+    if number_len ==0:
+        return('Неверный номер карты (счета)')
+    account_card_number = (account_card[-number_len:])
+    if number_len == 16:
+        return account_card.replace(account_card_number, masks.get_mask_card_number(int(account_card_number)))
+    else:
+        return account_card.replace(account_card_number, masks.get_mask_account(int(account_card_number)))
+
+
+print(mask_account_card("VisaPlatinum7000792289606361"))
