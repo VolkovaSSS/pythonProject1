4d45fd244dfd45b8852252da908166978f6f4c37
Sv <VolkovaSVG@mail.ru>
2025-10-26 16:39:34 +0400

    Add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -3,10 +3,6 @@ def filter_by_state(dictionaries: list[dict], state='EXECUTED')-> list[dict]:
     return [element for element in dictionaries if element.get('state') == state]
 
 
-test_list =[
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]
-print(filter_by_state(test_list, 'CANCELED'))
+def sort_by_date(dictionaries: list[dict], desc: bool=True)-> list[dict]:
+    """Сортирует список словарей по дате"""
+    return sorted(dictionaries, key = lambda x: x['date'], reverse=desc)
