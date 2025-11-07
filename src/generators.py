ae0042708f01b94f6c8d83a0ed02d3354d4ea56e
Sv <VolkovaSVG@mail.ru>
2025-11-07 19:25:40 +0400

    добавлены тесты к модулю generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,32 +1,39 @@
-from typing import Iterable
+from typing import Iterator
 
 
-def filter_by_currency(transacts: list[dict], currency: str) -> Iterable[dict]:
+def filter_by_currency(transacts: list[dict], currency: str) -> Iterator:
     """Фильтрует транзакции по валюте"""
 
-    for element in transacts:
-        if isinstance(element, dict):
-            if element.get("operationAmount", {}).get("currency", {}).get("code", {}) == currency:
-                yield element
+    if not isinstance(transacts, list):
+        raise TypeError()
+    return (
+        trans
+        for trans in transacts
+        if (
+            isinstance(trans, dict)
+            and trans.get("operationAmount", {}).get("currency", {}).get("code", {}) == currency
+        )
+    )
 
 
-def transaction_descriptions(transacts: list[dict]) -> Iterable[str]:
+def transaction_descriptions(transacts: list[dict]) -> Iterator:
     """Возвращает описание транзакции"""
+    if not isinstance(transacts, list):
+        raise TypeError("Некорректные данные")
+    for transaction in transacts:
+        if isinstance(transaction, dict):
+            yield transaction.get("description", "")
 
-    for element in transacts:
-        if isinstance(element, dict):
-            yield element.get("description", "")
 
-
-def card_number_generator(first_num, last_num) -> Iterable[str]:
+def card_number_generator(first_num, last_num) -> Iterator:
     """Генерирует номера карт"""
 
-    for num in range(first_num, last_num):
+    if not all([isinstance(first_num, int), isinstance(last_num, int)]):
+        raise TypeError("Номер счёта должен быть целым числом!")
+    if first_num > last_num:
+        raise ValueError("Конечный номер не может быть больше начального")
+    if first_num > 9999999999999999 or last_num > 9999999999999999 or first_num <= 0 or last_num <= 0:
+        raise ValueError("Номер должен быть в диапазоне от 0 до 9999999999999999")
+    for num in range(first_num, last_num + 1):
         num_str = str(num).zfill(16)
-        yield num_str[0:4] + " " + num_str[4:8] + " " + num_str[8:12] + " " + num_str[12:]
-
-
-
-
-
-
+        yield f"{num_str[:4]} {num_str[4:8]} {num_str[8:12]} {num_str[12:]}"
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -3,32 +3,6 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 
 @pytest.fixture
-def result_transactions_1():
-    return {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        }
-
-
-@pytest.fixture
-def result_transactions_2():
-    return {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-
-
-@pytest.fixture
 def input_transactions():
     return [
         {
@@ -49,6 +23,7 @@ def input_transactions():
             "from": "Счет 19708645243227258542",
             "to": "Счет 75651667383060284188",
         },
+        {},
         {
             "id": 873106923,
             "state": "EXECUTED",
@@ -85,13 +60,65 @@ def test_filter_by_currency(input_transactions):
     assert dict(next(usd_transactions)) == input_transactions[1]
 
 
+def test_filter_by_currency_empty():
+    usd_transactions = filter_by_currency([], "USD")
+    with pytest.raises(StopIteration):
+        next(usd_transactions)
+
+
+def test_filter_by_currency_no_match():
+    eur_transactions = filter_by_currency([], "EUR")
+    with pytest.raises(StopIteration):
+        next(eur_transactions)
+
+
+def test_filter_by_currency_wrong_type():
+    with pytest.raises(TypeError):
+        usd_transactions = filter_by_currency({"id": 142264268}, "USD")
+
+
 def test_transaction_descriptions(input_transactions):
     text_descriptions = transaction_descriptions(input_transactions)
-    assert str(next(text_descriptions)) == "Перевод со счета на счет"
+    assert str(next(text_descriptions)) == "Перевод организации"
     assert str(next(text_descriptions)) == "Перевод со счета на счет"
     assert str(next(text_descriptions)) == ""
 
 
-def test_card_number_generator():
-    for card_number in card_number_generator(1111222233334444, 1111222233334448):
-        print(card_number)
+def test_transaction_descriptions_empty():
+    text_descriptions = transaction_descriptions([])
+    with pytest.raises(StopIteration):
+        next(text_descriptions)
+
+
+
+def test_transaction_descriptions_wrong_type():
+    with pytest.raises(TypeError):
+        text_descriptions = transaction_descriptions()
+
+
+
+@pytest.mark.parametrize(
+    "first, last, expected",
+    [
+        (
+            1111222233334444,
+            1111222233334447,
+            ['1111 2222 3333 4444', '1111 2222 3333 4445', '1111 2222 3333 4446', '1111 2222 3333 4447'],
+        ),
+        (1, 1, ["0000 0000 0000 0001"]),
+    ],
+)
+def test_card_number_generator(first, last, expected):
+    for num in range(first, last):
+        assert list(card_number_generator(first, last)) == expected
+
+
+@pytest.mark.parametrize("first, last", [(0, 0), (99999999999999997, 99999999999999999), (-1, 4)])
+def test_card_number_generator_wrong_value(first, last):
+    with pytest.raises(ValueError):
+        it = list(card_number_generator(first, last))
+
+
+def test_card_number_generator_wrong_type():
+    with pytest.raises(TypeError):
+        it = list(card_number_generator('5', '1'))
