1b151965f57fc8aaf3962f0fa069e73200e15169
Sv <VolkovaSVG@mail.ru>
2025-11-09 22:44:49 +0400

    добавлены тесты к декоратору log

--- src/decorators.py
+++ src/decorators.py
@@ -4,20 +4,21 @@ from typing import Any, Callable, Optional
 
 
 def log(filename: Optional[str] = None) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    """Логирование выполнения функции"""
     def decorator(
         func: Callable[[Callable[..., Any]], Callable[..., Any]],
     ) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
         @wraps(func)
         def wrapper(*args, **kwargs):
             success = False
-            type_ex = None
             time_start = datetime.datetime.now()
             try:
                 result = func(*args, **kwargs)
                 log_text = f"{func.__name__} start {time_start} - ok"
                 success = True
-            except Exception as type_ex:
-                log_text = f"{func.__name__} start {time_start} error: {type_ex} inputs:{args}, {kwargs}"
+            except Exception as except_message:
+                error_text = str(except_message)
+                log_text = f"{func.__name__} start {time_start} error: {error_text} inputs:{args}, {kwargs}"
 
             if filename:
                 with open(filename, "a", encoding="utf-8") as file:
@@ -27,17 +28,8 @@ def log(filename: Optional[str] = None) -> Callable[[Callable[..., Any]], Callab
             if success:
                 return result
             else:
-                raise Exception(type_ex)
+                raise Exception(error_text)
 
         return wrapper
 
     return decorator
-
-
-@log(filename="mylog.txt")
-def my_function(x: int, y: int) -> float:
-    return x / y
-
-
-my_function(10, 2)
-my_function("1", 2)
